ac9a01d5aa2ff4052ea23d13caff8c44973f76af
Cesar 26 <cesarauge26@gmail.com>
2023-12-05 19:02:23 -0500

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const buttonEdit = document.querySelector(".profile__button");
 const popupProfile = document.querySelector(".popup_content_edit_profile");
 const closeButton = popupProfile.querySelector(".popup__button-close");
 
-function tooglePopup(popup) {
-  popup.classList.toggle("popup_show");
+const profileName = document.querySelector(".profile__name");
+const profilejob = document.querySelector(".profile__job");
+const formProfile = popupProfile.querySelector(".popup__form");
+const inputName = popupProfile.querySelector(".form__input[name="name")]");
+const inputjob = popupProfile.querySelector(".form__input[name="job")]");
 
+
+function tooglePopup(popup){
+  popup.classList.toggle("popup_show");
+}
   buttonEdit.addEventListener("click", function () {
     tooglePopup(popupProfile);
   });
@@ -12,4 +19,11 @@ function tooglePopup(popup) {
   closeButton.addEventListener("click", function () {
     tooglePopup(popupProfile);
   });
-}
+
+formProfile.addEventListener("submit", function(event){
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profilejob.textContent = inputjob.value;
+  formProfile.reset();
+  tooglePopup(popupProfile);
+})
