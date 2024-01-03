579d3ec4cb8626f090a39b31551ff772912d74e6
Cesar 26 <cesarauge26@gmail.com>
2024-01-02 20:28:48 -0500

    card pop up

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const inputName = popupProfile.querySelector(".form__input[name='name']");
 const inputjob = popupProfile.querySelector(".form__input[name='job']");
 
 
-function tooglePopup(popup){
+function tooglePopup(popup) {
   popup.classList.toggle("popup_show");
 }
-  buttonEdit.addEventListener("click", function () {
-    tooglePopup(popupProfile);
-  });
+buttonEdit.addEventListener("click", function () {
+  tooglePopup(popupProfile);
+});
 
-  closeButton.addEventListener("click", function () {
-    tooglePopup(popupProfile);
-  });
+closeButton.addEventListener("click", function () {
+  tooglePopup(popupProfile);
+});
 
-formProfile.addEventListener("submit", function(event){
+formProfile.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profilejob.textContent = inputjob.value;
@@ -28,28 +28,29 @@ formProfile.addEventListener("submit", function(event){
   tooglePopup(popupProfile);
 });
 
-
-
 const addButton = document.querySelector(".add-button");
 const popupaddElement = document.querySelector(".popup_content_add-element");
-const closeButtonNewPlace = popupaddElement.querySelector(".popup__button-close_new-place")
-const formProfileNewPlace = popupaddElement.querySelector(".popup__form_new-place");
-const inputNameNewPlace = popupaddElement.querySelector(".form__input[name='name']");
-const inputjobNewPlace = popupaddElement.querySelector(".form__input[name='job']");
+const closeButtonNewPlace = popupaddElement.querySelector(
+  ".popup__button-close"
+);
+const formProfileNewPlace = popupaddElement.querySelector(".popup__form");
+const inputNameNewPlace = popupaddElement.querySelector(
+  ".form__input[name='name']"
+);
+const inputjobNewPlace = popupaddElement.querySelector(
+  ".form__input[name='job']"
+);
 
 
-function tooglePopup(popup){
-  popup.classList.toggle("popup_show");
-}
-  addButton.addEventListener("click", function () {
-    tooglePopup(popupaddElement);
-  });
+addButton.addEventListener("click", function () {
+  tooglePopup(popupaddElement);
+});
 
-  closeButton.addEventListener("click", function () {
-    tooglePopup(popupaddElement);
-  });
+closeButtonNewPlace.addEventListener("click", function () {
+  tooglePopup(popupaddElement);
+});
 
-formProfile.addEventListener("submit", function(event){
+formProfile.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profilejob.textContent = inputjob.value;
@@ -60,52 +61,100 @@ formProfile.addEventListener("submit", function(event){
 const cards = [
   {
     name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
   },
   {
     name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
   },
   {
     name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
   },
   {
     name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
-  }
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
 ];
 
 const container = document.querySelector(".elements");
-cards.forEach(function (item){
-const template = document.querySelector("#card-template").content;
-const element = template.querySelector(".element").cloneNode(true);
+cards.forEach(function (item) {
+  const template = document.querySelector(".card-template").content;
+  const element = template.querySelector(".element").cloneNode(true);
+  
 
-const elementImage = element.querySelector(".element__image");
-const elementTitle = element.querySelector(".element__title");
+  const elementImage = element.querySelector(".element__image");
+  const elementTitle = element.querySelector(".element__title");
 
-const buttonDelete = element.querySelector(".button-delete");
-const buttonlike = element.querySelector(".element__like");
+  const buttonDelete = element.querySelector(".button-delete");
+  const buttonlike = element.querySelector(".element__like");
 
-elementImage.src = item.link
-elementTitle.innerText = item.name;
+  elementImage.src = item.link;
+  elementTitle.innerText = item.name;
 
-buttonDelete.addEventListener ("click", function (){
-  element.remove();
+  buttonDelete.addEventListener("click", function () {
+    element.remove();
+  });
+
+  buttonlike.addEventListener("click", function () {
+    buttonlike.classList.toggle("element__like-click");
+  });
+  
+  
+  elementImage.addEventListener("click", function () {
+  popup.classList.toggle("popup_show")
+  });
+
+
+  container.append(element);
+
+
+
+});
+/*
+formProfile.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profilejob.textContent = inputjob.value;
+  formProfile.reset();
+  tooglePopup(popupaddElement);
+  */
+ 
+  
+const popupImage = document.querySelector(".popup_content_image");
+
+
+
+popupImage.addEventListener("click", function (){
+popup.classList.toggle("popup_show");
+
+popupImage.append(elementImage)
+
 });
 
-elementLike.addEventListener("click", function() {
-  elementLike.classList.toggle("element__like-click")
-});
+/*
+addButton.addEventListener("click", function () {
+  tooglePopup(popupaddElement);
 });
 
+closeButtonNewPlace.addEventListener("click", function () {
+  tooglePopup(popupaddElement);
+});
 
+formProfile.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profilejob.textContent = inputjob.value;
+  formProfile.reset();
+  tooglePopup(popupaddElement);
+});
+*/
 
