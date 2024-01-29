119ea3783c4d1f4ef8c423123bad7a4e580988f6
Cesar 26 <cesarauge26@gmail.com>
2024-01-29 07:47:51 -0500

    first commit validate forms

--- validate.js
+++ validate.js
@@ -1,11 +1,64 @@
+const handleInput = (event, configForm) => {
+  const form = event.target.closest(configForm.formSelector);
+  const submitButton = form.querySelector(configForm.submitButtonSelector)
+  const target = event.target;
+  const errorNode = form.querySelector(`.${configForm.errorClass}${target.name}`);
+  if (target.validity.valid){
+    target.classList.remove(configForm.inputErrorClass);
+    errorNode.textContent = " ";
+  }else{
+    target.classList.add(configForm.inputErrorClass);
+    errorNode.textContent = target.validationMessage;
+  }
+  submitButton.disabled = !isvalid(form, configForm);
 
-const form = document.querySelector(".popup__form");
-const submitButton = form.querySelector(".form_submit")
+}
+
+function isvalid(form, configForm) {
+  const inputList = Array.from(form.querySelectorAll(configForm.inputSelector));
+  console.log(inputList, form, configForm.inputSelector);
+  return inputList.every((item)=>{
+    return item.validity.valid;
+  })
+}
 
 
+
+const enableValidation = (configForm) => {
+  const formList = Array.from(document.querySelectorAll(configForm.formSelector));
+  formList.forEach((formElement) => {
+    const submitButton = formElement.querySelector(configForm.submitButtonSelector)
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });  
+
+    formElement.addEventListener('input', event => {
+      handleInput(event, configForm);
+    });  
+    submitButton.disabled = !isvalid(formElement, configForm);
+  });
+}
+
+
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".form_input",
+  submitButtonSelector: ".form_submit",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "form_input_has-error",
+  errorClass: "form__error_index_"
+}); 
+
+
+
+//const form = document.querySelector(".popup__form");
+
+/*
 form.addEventListener("input", (event) => {
   const target = event.target;
   const errorNode = form.querySelector(`.form__error_index_${target.name}`);
+  console.log(errorNode, `.form__error_index_${target.name}`);
   if (target.validity.valid){
     target.classList.remove(".form_input_has-error");
     errorNode.textContent = " ";
@@ -14,35 +67,5 @@ form.addEventListener("input", (event) => {
     errorNode.textContent = target.validationMessage;
   }
 submitButton.disabled = !isvalid(form);
-
 })
-
-function isvalid(form) {
-  const inputList = Array.form(form.querySelectorAll("form_input"));
-  return inputList.every((item)=>{
-    return item.validity.valid;
-
-  })
-}
-submitButton.disabled = !isvalid(form);
-
-/*
-
-enableValidation({
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible"
-}); 
-
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form_input"));
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-    });
-  }
-  */
+*/
